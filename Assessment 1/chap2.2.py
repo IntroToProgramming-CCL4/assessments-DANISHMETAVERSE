fb8bca025b3a7ed838414f50a2a71742e65b6570
DANISHMETAVERSE <148261331+DANISHMETAVERSE@users.noreply.github.com>
2023-11-01 19:21:58 +0400

    Add files via upload

---
+++ Assessment 1/chap2.2.py
@@ -0,0 +1,3 @@
+quote = "I refuse to accept other people's ideas of happiness for me. As if there's a one size fits all standard for happiness"
+author = "Ye West"
+print(f'{author} once said, "{quote}"')
