b3969f13145219f0d45ad367e701e6cddcf267ae
DANISHMETAVERSE <148261331+DANISHMETAVERSE@users.noreply.github.com>
2023-11-08 19:55:58 +0400

    Update chap7.5.py

--- Assessment 1/Chapter 7- Functions/Exercises/chap7.5.py
+++ Assessment 1/Chapter 7- Functions/Exercises/chap7.5.py
@@ -1,8 +1,7 @@
-def describe_city(city, country='UAE'):
-    print (city,'is in the', country)
-describe_city ('London', 'United Kingdom')
-describe_city ('Abu Dhabi')
+def describe_city(city, country='UAE'): #Write a function called describe_city() that accepts the name of a city and its country.
+    print (city,'is in the', country) # The function should print a simple sentence, such as Reykjavik is in Iceland.
+describe_city ('London', 'United Kingdom') #Call your function for three different cities,
 describe_city ('Dubai')
 describe_city ('Reykjavik', 'Iceland')
 describe_city ('Manila', 'Philippines')
-describe_city ('Mumbai')
+describe_city ('Mumbai') #at least one of which is not in the default country.
