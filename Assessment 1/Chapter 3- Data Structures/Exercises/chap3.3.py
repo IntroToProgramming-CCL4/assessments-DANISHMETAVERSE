9021f5702df0f6d17aabd4d56e9515fd4449fa55
DANISHMETAVERSE <148261331+DANISHMETAVERSE@users.noreply.github.com>
2023-11-08 19:15:40 +0400

    Update chap3.3.py

--- Assessment 1/Chapter 3- Data Structures/Exercises/chap3.3.py
+++ Assessment 1/Chapter 3- Data Structures/Exercises/chap3.3.py
@@ -1,4 +1,4 @@
 modes_of_transportation = ['Jeepney', 'Audi r8', 'Lamborghini Urus', 'Kawasaki Motorcycle']
-
+#make a list that stores several examples of transportation
 for mode in modes_of_transportation:
-    print(f"I would like to own a {mode.capitalize()}.")
+    print(f"I would like to own a {mode.capitalize()}.") #print modes of transportation with message
