8f75e1c730c87ab2ba3b1b62a63641581a3450c7
DANISHMETAVERSE <148261331+DANISHMETAVERSE@users.noreply.github.com>
2023-11-08 19:25:28 +0400

    Update chap5.4.py

--- Assessment 1/Chapter 5- Dictionaries/Exercises/chap5.4.py
+++ Assessment 1/Chapter 5- Dictionaries/Exercises/chap5.4.py
@@ -1,7 +1,11 @@
+
 rivers = {'Nile River': 'Egypt', 'Niger River': 'Africa', 'Mississippi River': 'USA'}
 for key, value in rivers.items():
-    print ('\nThe', key, 'runs through', value)
-for key in rivers.keys():
+    print ('\nThe', key, 'runs through', value) 
+#Use a loop to print a sentence about each river.
+ for key in rivers.keys():
     print ('\nRiver name -', key)
+#Use a loop to print the name of each river included in the dictionary.
 for value in rivers.values():
     print (f'\nlocation -', value)
+#se a loop to print the name of each country included in the dictionary.
