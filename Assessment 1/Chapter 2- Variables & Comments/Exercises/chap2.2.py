5e4fa18c6de2d0e981d08a81f6ba5a624f06afac
DANISHMETAVERSE <148261331+DANISHMETAVERSE@users.noreply.github.com>
2023-11-08 19:01:20 +0400

    Update chap2.2.py

--- Assessment 1/Chapter 2- Variables & Comments/Exercises/chap2.2.py
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/chap2.2.py
@@ -1,3 +1,4 @@
+#quote from a person that i admire
 quote = "I refuse to accept other people's ideas of happiness for me. As if there's a one size fits all standard for happiness"
 author = "Ye West"
-print(f'{author} once said, "{quote}"')
+print(f'{author} once said, "{quote}"') #print the quote and the author
