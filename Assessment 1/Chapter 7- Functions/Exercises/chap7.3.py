812b561934e682beffb3ecdf61bafa06339cce65
DANISHMETAVERSE <148261331+DANISHMETAVERSE@users.noreply.github.com>
2023-11-08 19:53:06 +0400

    Update chap7.3.py

--- Assessment 1/Chapter 7- Functions/Exercises/chap7.3.py
+++ Assessment 1/Chapter 7- Functions/Exercises/chap7.3.py
@@ -1,4 +1,6 @@
-def make_shirt(size, message):
+def make_shirt(size, message): #Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
     print ('The size of the shirt is', size, 'and the message is', message)
 make_shirt ('Medium', 'dobi')
 make_shirt ('Small', 'janel')
+#Call the function once using positional
+#arguments to make a shirt. Call the function a second time using keyword arguments.
