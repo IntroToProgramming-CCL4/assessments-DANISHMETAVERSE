1b48fbc91390c9a9d8a4447e6d5a180fb9fcc2e8
DANISHMETAVERSE <148261331+DANISHMETAVERSE@users.noreply.github.com>
2023-11-08 19:44:11 +0400

    Update chap7.1.py

--- Assessment 1/Chapter 7- Functions/Exercises/chap7.1.py
+++ Assessment 1/Chapter 7- Functions/Exercises/chap7.1.py
@@ -1,3 +1,4 @@
-def display_message():
+def display_message():#prints one sentence telling everyone what you are learning about in this chapter
     print ('CHAPTER 7: FUNCTIONS. \nA function is a block of code which only runs when it is called. \nYou can pass data, known as parameters, into a function.')
-display_message()
+display_message() 
+#Call the function, and make sure the message displays correctly.
