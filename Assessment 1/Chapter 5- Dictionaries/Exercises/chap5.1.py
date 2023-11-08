c0f0d2318fa7fa6eafa4e704f7206d7500b75f2e
DANISHMETAVERSE <148261331+DANISHMETAVERSE@users.noreply.github.com>
2023-11-08 19:17:56 +0400

    Update chap5.1.py

--- Assessment 1/Chapter 5- Dictionaries/Exercises/chap5.1.py
+++ Assessment 1/Chapter 5- Dictionaries/Exercises/chap5.1.py
@@ -1,2 +1,3 @@
+# a dictionary to store information about a person you know.Store their first name, last name, age, and the city in which they live.
 friend = {'first name': 'Dhovie', 'last name': 'Costimiano', 'age':'', 'city':'Dubai'}
-print (friend)
+print (friend) #print info from dictionary
