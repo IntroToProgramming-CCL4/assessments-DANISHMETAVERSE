0b9ac92114d209c605ba873e91d580a6d71267bc
DANISHMETAVERSE <148261331+DANISHMETAVERSE@users.noreply.github.com>
2023-11-08 14:16:25 +0400

    Add files via upload
    SIR ETO PO YUNG CHAP 1.1 KO HINDI YUNG ISA : )

--- Assessment 1/Chapter 1- Getting Started with Python/Exercises/chap1.1.py
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/chap1.1.py
@@ -1,3 +1,6 @@
-#a python program to print the following string in a specific format. in this case, a lullaby
-
-print ("Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are")
+print ('Twinkle, Twinkle, Little star')
+print ('\tHow I wonder what you are')
+print ('\t\tUp above the world so high')
+print ('\t\tLike a diamond in the sky')
+print ('Twinkle, Twinkle, Little star')
+print ('\tHow I wonder what you are')
