38d37e6bded34277a467f6187ac1a625922bc116
DANISHMETAVERSE <148261331+DANISHMETAVERSE@users.noreply.github.com>
2023-11-08 19:46:28 +0400

    Update chap7.2.py

--- Assessment 1/Chapter 7- Functions/Exercises/chap7.2.py
+++ Assessment 1/Chapter 7- Functions/Exercises/chap7.2.py
@@ -1,4 +1,4 @@
-def favorite_book(title):
-    print ('One of my favorite book is', (title))
-favorite_book('The Alchemist')
+def favorite_book(title):#Write a function called favorite_book() that accepts one parameter, title
+    print ('One of my favorite book is', (title)) #The function should print a message, such as One of my favorite books is Alice in Wonderland.
+favorite_book('The Alchemist') 
 favorite_book('To Kill a Mockingbird')
