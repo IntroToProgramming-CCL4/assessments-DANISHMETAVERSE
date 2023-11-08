fd75a2bcee9d0be19859175c769be9aa05cb48d6
DANISHMETAVERSE <148261331+DANISHMETAVERSE@users.noreply.github.com>
2023-11-08 19:31:13 +0400

    Update chap6.1.py

--- Assessment 1/Chapter 6- Loops/Exercises/chap6.1.py
+++ Assessment 1/Chapter 6- Loops/Exercises/chap6.1.py
@@ -1,6 +1,7 @@
+#a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value.
 toppings= input('What topping would you like to add:')
 while toppings != 'quit':
-    print ('I will add', toppings, 'to your pizza!')
+    print ('I will add', toppings, 'to your pizza!') #print a message saying you add the 
     toppings = input ('would you like to add more?:')
 else:
     print ('Thank you, have a great day.')
