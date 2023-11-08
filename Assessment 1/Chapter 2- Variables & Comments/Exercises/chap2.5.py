1f1a190d0773a8741b4abc5d61f4de16056fe40f
DANISHMETAVERSE <148261331+DANISHMETAVERSE@users.noreply.github.com>
2023-11-08 19:09:48 +0400

    Update chap2.5.py

--- Assessment 1/Chapter 2- Variables & Comments/Exercises/chap2.5.py
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/chap2.5.py
@@ -4,3 +4,5 @@ num_usb_sticks = budget // usb_stick_price
 leftover_pounds = budget % usb_stick_price
 print(f"She can buy {num_usb_sticks} USB sticks.")
 print(f"She will have {leftover_pounds} pounds left.")
+#program that calculates how many usb sticks she can buy and 
+#how much money she has left using arithmetic operators
