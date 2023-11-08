2b2b4c2ee89411899af26cc3726daed3ba6ed5cb
DANISHMETAVERSE <148261331+DANISHMETAVERSE@users.noreply.github.com>
2023-11-08 20:39:53 +0400

    Update chap7.4.py

--- Assessment 1/Chapter 7- Functions/Exercises/chap7.4.py
+++ Assessment 1/Chapter 7- Functions/Exercises/chap7.4.py
@@ -1,6 +1,6 @@
-def make_shirt(size='Large', message='Programming'):
+def make_shirt(size='Large', message='I Love Python'):
     print ('The size of the shirt is', size, 'and the message is', message)
-make_shirt ('"Medium"', '"dobi"')
-make_shirt ('"Small"', '"janel"')
+make_shirt ('"Medium"', '"I Love python"') #Make a large shirt and a medium shirt with the default message
+make_shirt ('"Small"', '"dobi"') #and a shirt of any size with a different message.
 make_shirt()
 make_shirt('Medium')
