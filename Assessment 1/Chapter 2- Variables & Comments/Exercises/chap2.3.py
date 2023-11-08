dd35e75018f0e0378daf5cec67b74b14cb015340
DANISHMETAVERSE <148261331+DANISHMETAVERSE@users.noreply.github.com>
2023-11-08 19:06:44 +0400

    Update chap2.3.py

--- Assessment 1/Chapter 2- Variables & Comments/Exercises/chap2.3.py
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/chap2.3.py
@@ -1,9 +1,9 @@
-name = "  \t\n Danish Baloch \t\n"
-print("Original Name:")
+name = "  \t\n Danish Baloch \t\n" #person name with whitespace at beginning and end
+print("Original Name:") #print once so whitespace is displayed
 print(name)
-print("\nAfter using lstrip():")
+print("\nAfter using lstrip():") #print using 3 stripping functions. LSTRIP
 print(name.lstrip())
-print("\nAfter using rstrip():")
+print("\nAfter using rstrip():") #RSTRIP
 print(name.rstrip())
-print("\nAfter using strip():")
+print("\nAfter using strip():") #STRIP
 print(name.strip())
